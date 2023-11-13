e413259fcd25c4778fb011d78aad151378fcf0e1
LAPTOP-I84T5CJS\admin <kumbharshubham0207@gmail.com>
2023-11-13 13:08:44 +0530

    Commit the files for duct design pallette UI - Shubham

---
+++ DuctPalletteDesign/DuctPalletteDesign/MVVM/View Model/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DuctPalletteDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuctPalletteDesign.MVVM.View_Model
+{
+    class MainViewModel:ObservableObject
+    {
+
+        public DrawingDetails drawingDetails { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; }
+        }
+
+        public MainViewModel()
+        {
+            drawingDetails = new DrawingDetails();
+            CurrentView = drawingDetails;
+        }
+    }
+}
